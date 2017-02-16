503f10c7f05a7531b36b1cb38110106459968c1a
meatechie <kranthi.pullagurla@gmail.com>
2017-02-15 21:01:35 -0600

    Update LAMPScript-BareMetal_CentOS_7.2.sh

--- LAMPScript-BareMetal_CentOS_7.2.sh
+++ LAMPScript-BareMetal_CentOS_7.2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 echo "Running LAMP Script for Centos 7.2!"
 echo "*********** Change Firewall to allow HTTP and HTTPS traffic**********"
 sudo firewall-cmd --permanent --zone=public --add-service=http
