5227ce1075df0f371e5a741a78e115ffcb3ddfbf
meatechie <kranthi.pullagurla@gmail.com>
2017-02-15 20:46:37 -0600

    Update LAMPScript-BareMetal_CentOS_7.2.sh

--- LAMPScript-BareMetal_CentOS_7.2.sh
+++ LAMPScript-BareMetal_CentOS_7.2.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
-
 echo "Running LAMP Script for Centos 7.2!"
-
 echo "*********** Change Firewall to allow HTTP and HTTPS traffic**********"
-sudo firewall-cmd --permanent --zone=public --add-service=http 
+sudo firewall-cmd --permanent --zone=public --add-service=http
 sudo firewall-cmd --permanent --zone=public --add-service=https
 sudo firewall-cmd --reload
 
@@ -13,7 +10,6 @@ sudo yum update
 
 echo "*********** Install Apache Packages **********"
 sudo yum install httpd
-
 echo "*********** Start Apache Service and enable it to start on boot **********"
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
@@ -21,7 +17,6 @@ sudo systemctl restart httpd.service
 
 echo "*********** Install MySQL (MariaDB) Packages. MariaDB is replacing MySQL and its the fork of MySQL**********"
 sudo yum install mariadb-server mariadb
-
 echo "*********** Start MySQL (MariaDB) Service and enable it to start on boot **********"
 sudo systemctl start mariadb.service
 sudo systemctl enable mariadb.service
